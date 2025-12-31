7c498b30fbd3b20bb625b2511ce20a2fa2beb5e4
valentingenev <valentin_genev@abv.bg>
2025-12-31 16:42:01 +0200

    filter unique games too

--- src/scripts/read-moves.ts
+++ src/scripts/read-moves.ts
@@ -3,11 +3,12 @@ import { GameReader } from "../GameReader";
 import * as fs from 'fs';
 
 const movesData: Array<CellIndex[]> = JSON.parse(fs.readFileSync('data/unique-moves.json', 'utf-8'))
-const games = new Array<number[]>(movesData.length)
+const gamesAsJson = new Array<string>(movesData.length)
 
 for (const i in movesData) {
-    games[i] = new GameReader(movesData[i]).getGame()
+    gamesAsJson[i] = JSON.stringify(new GameReader(movesData[i]).getGame())
 }
+const games = [...new Set(gamesAsJson)].map(g => JSON.parse(g))
 
 fs.mkdirSync('data', { recursive: true })
 fs.writeFileSync('data/games.json', JSON.stringify(games, null))
