0e60ab0c48a773095ad3693c50df32b63bcdf7ba
valentingenev <valentin_genev@abv.bg>
2025-12-31 16:46:24 +0200

    filtration step is now part of the generation

--- src/scripts/generate-moves.ts
+++ src/scripts/generate-moves.ts
@@ -1,16 +1,17 @@
-import { CellIndex } from "../model/CellIndex"
 import { MovesGenerator } from "../MovesGenerator"
 import * as fs from 'fs';
 
 // After 1500000 the grow of the number of unique games starts to slow
 const gamesCount = 1500000
-const movesSets = new Array<CellIndex[]>(gamesCount)
+const movesSets = new Array<string>(gamesCount)
 for (let i = 0; i < movesSets.length; i++) {
     const generator = new MovesGenerator()
-    movesSets[i] = generator.doUntilFull()
+    movesSets[i] = JSON.stringify(generator.doUntilFull())
 }
+const uniqueMovesSets = [...new Set(movesSets)].map(ms => JSON.parse(ms))
 
 fs.mkdirSync('data', { recursive: true })
-fs.writeFileSync('data/moves.json', JSON.stringify(movesSets, null))
+fs.writeFileSync('data/moves.json', JSON.stringify(uniqueMovesSets, null))
 
 console.info('INFO ---------------', `Generated move sets: ${gamesCount}`)
+console.info('INFO ---------------', `Stored unique sets: ${uniqueMovesSets.length}`)
